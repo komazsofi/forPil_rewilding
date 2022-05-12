0212f60d0680de1dd619a704d829580f543e078f
Zsofia Koma <komazsofi@gmail.com>
2022-05-12 13:41:44 +0200

    clan up script

--- prep_rasters_formatchit.R
+++ prep_rasters_formatchit.R
@@ -8,7 +8,7 @@ library(plyr)
 
 library(MatchIt)
 
-source("C:/_Koma/GitHub/komazsofi/forPil_rewilding/Derek_SplitRaster.R")
+source("C:/_Koma/GitHub/komazsofi/forPil_rewilding/Derek_SplitRaster.R") # need to be changed to the absolute path where the file is located
 
 workingdirectory="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/forPil/Project_1/"
 setwd(workingdirectory)
@@ -26,43 +26,53 @@ predictors_crop=crop(predictors,e)
 
 ### Split raster and convert to data frame
 
-setwd(paste0(workingdirectory,"/","processing_withmachit","/"))
+setwd(paste0(workingdirectory,"/","processing_test2","/"))
+
+old <- Sys.time()
 
 SplitRas(Raster = predictors_crop, ppside = 4, nclus = 2)
 Files <- list.files(pattern = "SplitRas", full.names = T)
 
-DF <- SplitsToDataFrame(Splits = Files, ncores = 2)
+new <- Sys.time() - old 
+print(new) 
 
-### Clean up the data frame for matching # move up renaming
+old2 <- Sys.time()
 
-colnames(DF)<-c("UTM_X","UTM_Y",filelist)
-write.csv2(DF,"forMatching.csv")
+DF_complete_cases <- SplitsToDataFrame(Splits = Files, ncores = 2)
 
-DF_complete_cases=DF[complete.cases(DF),] # this is not necessary
+new2 <- Sys.time() - old2 
+print(new2) 
+
+### Clean up the data frame for matching 
+
+colnames(DF_complete_cases)<-c("UTM_X","UTM_Y",filelist)
+
+names(DF_complete_cases)<-c("UTM_X","UTM_Y","Nature_types","Size","AMP","TWI","MDI","DTM","NDVI_before","NDVI_max",
+                            "Habitat_nature","MPD","Rewilding_id","Treatment","RZC","Slope")
 
 # scientific clean up
 
 # rewilding or not
-DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif[DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif<10]<-0
-DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif[DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif==10]<-1
+DF_complete_cases$Treatment[DF_complete_cases$Treatment<10]<-0
+DF_complete_cases$Treatment[DF_complete_cases$Treatment==10]<-1
 
 # add nat types with name
-DF_complete_cases$Article3_NATYPE_raster_10m_utm32.tif<-as.factor(DF_complete_cases$Article3_NATYPE_raster_10m_utm32.tif)
-DF_complete_cases$Article3_NATYPE_raster_10m_utm32.tif<-revalue(DF_complete_cases$Article3_NATYPE_raster_10m_utm32.tif,
+DF_complete_cases$Nature_types<-as.factor(DF_complete_cases$Nature_types)
+DF_complete_cases$Nature_types<-revalue(DF_complete_cases$Nature_types,
                                                      c("1"="Freshwater meadow", "2"="Heathland", "3"="Bog", "4"="Grassland","5"="s","6"="Lake","7"="Coastal meadow"))
 
 # what type of rewilding # need to put the UTF-8
-DF_complete_cases$Rewilding_ID_inclNA_10m_utm32.tif<-as.factor(DF_complete_cases$Rewilding_ID_inclNA_10m_utm32.tif)
+DF_complete_cases$Rewilding_id<-as.factor(DF_complete_cases$Rewilding_id)
 
-DF_complete_cases$Rewilding_ID_inclNA_10m_utm32.tif<-revalue(DF_complete_cases$Rewilding_ID_inclNA_10m_utm32.tif,
-                                                      c("0"="Article 3",
-                                                        "1"="Saksfjed-Hyllekrog", "2"="Tofte", "3"="Mellemomr?det",
-                                                        "4"="Molslaboratoriet","5"="Klise Nor","6"="Dovns Klint",
-                                                        "7"="Almindingen", "8"="B?t?skoven", "9"="Brands?",
-                                                        "10"="Skavenhus", "11"="Ulsvhale Nord", "12"="Ulvshale Syd",
-                                                        "13"="Klelund", "14"="Geding Kasted Mose", "15"="Merritskoven",
-                                                        "16"="Fl?det", "17"="Stengade", "18"="T?r?",
-                                                        "19"="N?stved ?velsesterr?n", "20"="N?rrestrand Horsens"))
+DF_complete_cases$Rewilding_id<-revalue(DF_complete_cases$Rewilding_id,
+                                        c("0"="Article 3",
+                                          "1"="Saksfjed-Hyllekrog", "2"="Tofte", "3"="Mellemområdet",
+                                          "4"="Molslaboratoriet","5"="Klise Nor","6"="Dovns Klint",
+                                          "7"="Almindingen", "8"="Bøtøskoven", "9"="Brandsø",
+                                          "10"="Skavenhus", "11"="Ulsvhale Nord", "12"="Ulvshale Syd",
+                                          "13"="Klelund", "14"="Geding Kasted Mose", "15"="Merritskoven",
+                                          "16"="Flådet", "17"="Stengade", "18"="Tærø",
+                                          "19"="Næstved Øvelsesterræn", "20"="Nørrestrand Horsens"))
 
 
 write.csv2(DF_complete_cases,"forMatching_cleaned.csv")
@@ -70,19 +80,21 @@ write.csv2(DF_complete_cases,"forMatching_cleaned.csv")
 # check dimensions
 dim(DF_complete_cases)
 str(DF_complete_cases)  
-levels(as.factor(DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif))
-nrow(DF_complete_cases[DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif==1,])
-nrow(DF_complete_cases[DF_complete_cases$Rewilding_PA_combined_10m_utm32.tif!=1,])
+levels(as.factor(DF_complete_cases$Treatment))
+nrow(DF_complete_cases[DF_complete_cases$Treatment==1,])
+nrow(DF_complete_cases[DF_complete_cases$Treatment!=1,])
 
 ### Matching - modelling
 
-names(DF_complete_cases)<-c("UTM_X","UTM_Y","Nature_types","Size","AMP","TWI","MDI","DTM","NDVI_before","NDVI_max",
-                            "Habitat_nature","MPD","Rewilding_id","Treatment","RZC","Slope")
-
 # with NDVI
+
+old3 <- Sys.time()
+
 rewilding_match_PSM_output <- matchit(Treatment~Nature_types + AMP + TWI + MDI + DTM + NDVI_before + Habitat_nature + RZC + Slope,
                                       data =DF_complete_cases, replace=FALSE, caliper = 0.25, method = "nearest", exact= "Nature_types", distance = "logit")
 
+new3 <- Sys.time() - old3 
+print(new3) 
 
 plot(summary(rewilding_match_PSM_output))
 m.data <- match.data(rewilding_match_PSM_output)
